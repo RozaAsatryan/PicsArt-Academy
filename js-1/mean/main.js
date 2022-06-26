72ea20f64b7fe9dc6f30096d149502df0cc4c6a2
Roza <roza.asatryan.04|@mail.ru>
2022-06-27 01:41:08 +0400

    not finished again

--- js-1/mean/main.js
+++ js-1/mean/main.js
@@ -1,5 +1,13 @@
-let sum = 0;
+debugger;
+let rem = 0,
+  sum = 0;
 
-function mean(meanValue) {
-    sum = meanValue[i]
+function mean(num) {
+  while (num > 0) {
+    rem = num % 10; //vor vercni verjin tiv@
+    sum = sum + rem; //gumari sum-in
+    num = num / 10; //hani ayd verjin tiv@
+  }
 }
+
+console.log(mean(561));
