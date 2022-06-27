32561ef0eb637af268faa564edcbd494de48a66a
Roza <roza.asatryan.04|@mail.ru>
2022-06-27 16:58:37 +0400

    finish

--- js-1/specialArray/main.js
+++ js-1/specialArray/main.js
@@ -7,18 +7,17 @@ let i = 0;
  */
 
 function isSpecialArray(arr) {
+  let i = 0;
   while (i < arr.length) {
-    if (arr[i] % 2 == 0 && i % 2 == 0) {
-      return true;
-    } else {
+    if (arr[i] % 2 !== i % 2) {
       return false;
     }
     i++;
   }
+  return true;
 }
 
-console.log(isSpecialArray([0, 5, 8, 1, 33, 1, 6, 3])); //return true
+console.log(isSpecialArray([0, 5, 8, 1, 2, 1, 6, 3])); //return true
 console.log(isSpecialArray([1, 3, 42, 98, 6, 21, 46, 333])); //return false
 
 console.log(isSpecialArray([2, 7, 9, 1, 6, 1, 6, 3])); // return false,
-// Tigran jan why doesn't work in this example?
